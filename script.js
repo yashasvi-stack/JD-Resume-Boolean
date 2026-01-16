9544d865fc9b6e7c67a8de3447042ca82bdd16a1
Vignesh S <DK2835@desicrew.in>
2026-01-16 10:12:26 +0530

    refresh button updated

--- script.js
+++ script.js
@@ -68,13 +68,6 @@
 //   document.getElementById("themeIcon").textContent = isLight ? "☀️" : "🌙";
 // }
 
-function toggleTheme() {
-  document.body.classList.toggle("light");
-
-  const toggle = document.querySelector(".theme-toggle");
-  toggle.textContent = document.body.classList.contains("light") ? "🌞" : "🌙";
-}
-
 
 // Load saved theme or system preference
 (function initTheme() {
